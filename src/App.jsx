2bb7237e5823a3e9b8c32e0301a506fb90e4353f
rakibhossainofficial <rakib20@gmail.com>
2024-09-16 11:56:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import BlogHome from './Components/BlogHome'
+
+
+
+function App() {
+
+  return (
+    <>
+      <BlogHome />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/BlogHome.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const BlogHome = () => {
+
+    const data = [
+        {
+          id: 1,
+          titleOne: "YOU CAN CHANGE THE WORLD",
+          titleTwo: "STRONG STYLE",
+          paraOne: "How Women are Redirecting Hollywood",
+          paraTwo: "New Ways to Wear Denim Belt Bags make a Comeback",
+          name: "By Jasmin Smith",
+          subTitle: "How to Shop Mindfully",
+          qrCode: "/images/qrcode.png",
+          cover: "/images/blog5.jpg",
+        },
+    ]
+
+  return (
+    <>
+        <section className='home'>
+            <div className="left-content">
+                {
+                    data.map((value) => {
+                        return(
+                            <div className="content">
+                                <div className="logo">
+                                    <h1>M</h1>
+                                </div>
+                                <div className="home-img">
+                                    <img src={value.cover} alt='' />
+                                </div>
+                                <div className="text">
+                                    <h1>{value.titleOne}</h1>
+                                    <p>{value.paraOne}</p>
+                                    <span>{value.name}</span>
+                                </div>
+                                <div className="text text2">
+                                    <h1>{value.titleTwo}</h1>
+                                    <p>{value.paraTwo}</p>
+                                    <span>{value.subTitle}</span>
+                                </div>
+                                <div className="qrcode">
+                                    <img src={value.qrCode} alt='' />
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="right-content"></div>
+        </section>
+    </>
+  )
+}
+
+export default BlogHome
